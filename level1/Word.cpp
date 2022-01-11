16e7ccef9344480d6aa0b9e5a36f57a23b98bd81
Azedine Ouhadou <aouhadou@e1r3p2.1337.ma>
2022-01-11 18:19:27 +0100

    Magnets Codeforces problem Solution

---
+++ level1/Word.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int i, j, low = 0, up = 0;
+    string  str;
+    cin >> str;
+    int len = str.length();
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            low += 1;
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            up += 1;
+    }
+
+   if (up > low)
+    {
+        string nstr;
+        for (j = 0; j < len; j++)
+        {
+            nstr = toupper(str[j]);
+            cout << nstr;
+        }
+    }
+    else
+    {
+        string nstr;
+        for (j = 0; j < len; j++)
+        {
+            nstr = tolower(str[j]);
+            cout << nstr;
+        }
+    }
+    return 0;
+}
